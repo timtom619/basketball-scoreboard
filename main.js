4b4fc6d1e2daccf424b0b917feba5d6c1226032d
Timothy Tomas <timothyjtee@gmail.com>
2023-03-26 10:56:49 -0700

    implement input point logic to scoreboard

---
+++ main.js
@@ -0,0 +1,62 @@
+const teamScore = document.querySelector(".team-score");
+const guestScore = document.querySelector(".guest-score");
+
+// Add one logic
+let btnOneHome = document.getElementById("btn1-home");
+btnOneHome.addEventListener("click", addOne);
+
+let btnOneGuest = document.getElementById("btn1-guest");
+btnOneGuest.addEventListener("click", addOne);
+
+function addOne(e) {
+  let currentValue;
+  if(e.srcElement.id.includes("home")) {
+    currentValue = Number(teamScore.textContent)
+    currentValue += 1;
+    teamScore.textContent = currentValue;
+  } else {
+    currentValue = Number(guestScore.textContent)
+    currentValue += 1;
+    guestScore.textContent = currentValue;
+  }
+}
+
+// Add two logic
+let btnTwoHome = document.getElementById("btn2-home");
+btnTwoHome.addEventListener("click", addTwo);
+
+let btnTwoGuest = document.getElementById("btn2-guest");
+btnTwoGuest.addEventListener("click", addTwo);
+
+function addTwo(e) {
+  let currentValue;
+  if(e.srcElement.id.includes("home")) {
+    currentValue = Number(teamScore.textContent)
+    currentValue += 2;
+    teamScore.textContent = currentValue;
+  } else {
+    currentValue = Number(guestScore.textContent)
+    currentValue += 2;
+    guestScore.textContent = currentValue;
+  }
+}
+
+// Add three logic
+let btnThreeHome = document.getElementById("btn3-home");
+btnThreeHome.addEventListener("click", addThree);
+
+let btnThreeGuest = document.getElementById("btn3-guest");
+btnThreeGuest.addEventListener("click", addThree);
+
+function addThree(e) {
+  let currentValue;
+  if(e.srcElement.id.includes("home")) {
+    currentValue = Number(teamScore.textContent)
+    currentValue += 3;
+    teamScore.textContent = currentValue;
+  } else {
+    currentValue = Number(guestScore.textContent)
+    currentValue += 3;
+    guestScore.textContent = currentValue;
+  }
+}
